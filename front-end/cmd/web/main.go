68882642dab640f9fb7a7ac3c7e7f7d66b843e0f
Blagodatskih Serge <stikkas17@gmail.com>
2023-07-29 14:11:36 +0300

    microservices in go

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+	var port = 8000
+	fmt.Printf("Starting front end service http://localhost:%v\n", port)
+	log.Panic(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, partials...)
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
