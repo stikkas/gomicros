102dcc91ab17d533a3828bede52cd528d10959ae
Blagodatskih Serge <stikkas17@gmail.com>
2023-08-19 11:13:47 +0300

    mail service in docker

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", webPort)
 
@@ -22,4 +27,20 @@ func main() {
 	}
 
 	panic(srv.ListenAndServe())
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_NAME"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
+	}
+
+	return m
 }
