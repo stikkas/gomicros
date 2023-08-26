160f57b58eb07bdd2407aebd0c1913559149f260
Blagodatskih Serge <stikkas17@gmail.com>
2023-08-26 09:02:34 +0300

    rpc is working

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -46,6 +48,11 @@ func main() {
 	app := Config{
 		Models: data.New(client),
 	}
+
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	app.serve()
 }
 
@@ -56,6 +63,22 @@ func (app *Config) serve() {
 	}
 
 	panic(srv.ListenAndServe())
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
